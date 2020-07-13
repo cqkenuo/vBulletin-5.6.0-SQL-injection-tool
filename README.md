e66514077fc5bbde21a4803a6fce0fdd6bb600c7
纵饮孤独 <3071597936@qq.com>
2020-07-13 16:42:39 +0800

    update

---
+++ README.md
@@ -0,0 +1,3 @@
+本工具由 QJ 开发 zygds(Gith ub/ee id)维护
+
+欢迎加入HW QQ群，本群大力弘扬互联网分享精神
--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from tools import *
 from get_data import *
 
 if __name__ == '__main__':
+    print("本工具由QJ开发，zygds(GitHub id)维护")
+    print("开发日期：2020：07：12")
+    print("更新日期：")
     url = input('请输入需要测试的网址：')
     print("***********正在查询当前数据库***********")
     get_database(url)
